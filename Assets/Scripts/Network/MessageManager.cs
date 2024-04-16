f620778b61fb14ac7a2ddd85001510575c53c021
fekox <facusantos123@gmail.com>
2024-04-15 22:09:03 -0300

    Add MessageManager script

---
+++ Assets/Scripts/Network/MessageManager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.VersionControl;
+using UnityEngine;
+
+public class MessageManager : MonoBehaviour
+{
+    public void ClientOrServerCheck()
+    {
+        if (NetworkManager.Instance.isServer)
+        {
+
+        }
+
+        else
+        {
+
+        }
+    }
+
+    public void ReadMessage(MessageType messageType) 
+    {
+        switch (messageType) 
+        {
+            case MessageType.HandShake:
+            
+            break;
+
+            case MessageType.Position:
+
+            break;
+
+            case MessageType.String:
+
+            break;
+
+            case MessageType.Console:
+
+            break;
+        }
+    }
+}
