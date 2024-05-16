220f3ed22b9e3120bb31120a543584da271b065e
fekox <facusantos123@gmail.com>
2024-05-16 12:47:45 -0300

    Add the new input systems package

---
+++ Assets/Scripts/Player/PlayerInputManager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInputManager : MonoBehaviour
+{
+    //public void OnMove(InputValue value)
+    //{
+    //    playerMovement.Move(value);
+    //}
+
+    //public void OnShoot()
+    //{
+    //    playerShoot.ShootLogic();
+    //}
+}
